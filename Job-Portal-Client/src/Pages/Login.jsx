29f1d54e23b370a55d3054bddbdc5b7e779cc015
ShamimHossain1 <shamimweb78@gmail.com>
2025-03-29 03:08:36 -0400

    animation to login and register page

--- Job-Portal-Client/src/Pages/Login.jsx
+++ Job-Portal-Client/src/Pages/Login.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
-
+import Lottie from "lottie-react";
+import animationData from "../../public/signup_animation.json";
 const Login = () => {
-    return (
-        <div>
-            <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-    <h1 className="text-5xl font-bold text-center py-4">Register now!</h1>
-      <div className="card-body">
-        <fieldset className="fieldset">
-          <label className="fieldset-label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="fieldset-label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left w-96">
+            <Lottie animationData={animationData}></Lottie>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <h1 className="text-5xl font-bold text-center py-4">Login now!</h1>
+            <div className="card-body">
+              <fieldset className="fieldset">
+                <label className="fieldset-label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="fieldset-label">Password</label>
+                <input type="password" className="input" placeholder="Password" />
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Login;
--- Job-Portal-Client/src/Pages/Register.jsx
+++ Job-Portal-Client/src/Pages/Register.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import animationData from "../../public/signup_animation.json";
 
 const Register = () => {
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen ">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                      
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
+                    <div className="text-center lg:text-left w-96">
+                        <Lottie animationData={animationData}></Lottie>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <h1 className="text-5xl font-bold text-center py-4">Register now!</h1>
-                        <div className="card-body">
-                            <fieldset className="fieldset">
-                                <label className="fieldset-label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
-                                <label className="fieldset-label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
-                                <div><a className="link link-hover">Forgot password?</a></div>
-                                <button className="btn btn-neutral mt-4">Login</button>
-                            </fieldset>
-                        </div>
+                        <h1 className="text-5xl font-bold text-center py-4">Register now!</h1>
+                        <form onSubmit={handleRegister} className="card-body">
+
+                            <label className="fieldset-label">Email</label>
+                            <input type="email" name='email' className="input" placeholder="Email" />
+                            <label className="fieldset-label">Password</label>
+                            <input name='password' type="password" className="input" placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button type='submit' className="btn btn-neutral mt-4">Register</button>
+
+                        </form>
                     </div>
                 </div>
             </div>
